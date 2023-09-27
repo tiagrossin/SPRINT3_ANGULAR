fefc6bad064e2bd0c3ec3beac7af5b1846fd6962
tiagrossin <tiagoyjose@gmail.com>
2023-09-27 09:47:04 +0200

    corrections

--- js/checkout.js
+++ js/checkout.js
@@ -20,12 +20,12 @@ function validate(event) {
     var inputField = document.getElementById(field.id);
     var errorElement = document.getElementById("error" + field.id.substring(1));
     if (inputField.value.length < 3 || !field.regex.test(inputField.value)) {
-      errorElement.style.display = "block"; // Si es incorrecto mostramos el mensaje de error
-      inputField.classList.add("is-invalid"); //Añadimos la clase is-invalid para indicar visualmente el error
+      errorElement.style.display = "block";
+      inputField.classList.add("is-invalid");
     } else {
-      errorElement.style.display = "none"; // Si el formato del campo esta bien, ocultamos el mensaje de error
-      inputField.classList.remove("is-invalid"); // y eliminamos la clase is-invalid
-      inputField.classList.add("is-valid"); // Dejamos el tick verde en caso correcto.
+      errorElement.style.display = "none";
+      inputField.classList.remove("is-invalid");
+      inputField.classList.add("is-valid");
     }
   });
 }
