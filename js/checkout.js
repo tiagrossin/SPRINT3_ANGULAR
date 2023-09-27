30b1842d850ba23af59e536c277ff3b11de20096
tiagrossin <tiagoyjose@gmail.com>
2023-09-27 09:46:20 +0200

    excercise 7

--- js/checkout.js
+++ js/checkout.js
@@ -1,32 +1,31 @@
-
 // Exercise 6
 function validate(event) {
-	//Regular expresions filter
-	var lettersOnly = /^[A-Za-z]+$/;
-    var emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{1,3})+$/;
-    var numberFormat = /^\d{9}$/;
-    var passwordFormat = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/;
-	// Get the input fields
-	var fName = document.getElementById("fName");
-	var fEmail = document.getElementById("fEmail");
+  event.preventDefault();
+  //Regular expresions filter
+  var lettersOnly = /^[A-Za-z]+$/;
+  var emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{1,3})+$/;
+  var numberFormat = /^\d{9}$/;
+  var passwordFormat = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/;
 
-	// Get the error elements
-	var errorName = document.getElementById("errorName");
-	var errorEmail = document.getElementById("errorEmail");  
-	
-	// Validate fields entered by the user: name, phone, password, and email
-	if(fName.value == ""){
-		error++;
-	}
+  var fields = [
+    { id: "fName", regex: lettersOnly },
+    { id: "fLastN", regex: lettersOnly },
+    { id: "fEmail", regex: emailFormat },
+    { id: "fPassword", regex: passwordFormat },
+    { id: "fAddress", regex: /.+/ },
+    { id: "fPhone", regex: numberFormat },
+  ];
 
-	if(fEmail.value == ""){
-		error++;
-	}
-	 
-	if(error>0){
-		alert("Error");
-	}else{
-		alert("OK");
-	}
-
+  fields.forEach((field) => {
+    var inputField = document.getElementById(field.id);
+    var errorElement = document.getElementById("error" + field.id.substring(1));
+    if (inputField.value.length < 3 || !field.regex.test(inputField.value)) {
+      errorElement.style.display = "block"; // Si es incorrecto mostramos el mensaje de error
+      inputField.classList.add("is-invalid"); //Añadimos la clase is-invalid para indicar visualmente el error
+    } else {
+      errorElement.style.display = "none"; // Si el formato del campo esta bien, ocultamos el mensaje de error
+      inputField.classList.remove("is-invalid"); // y eliminamos la clase is-invalid
+      inputField.classList.add("is-valid"); // Dejamos el tick verde en caso correcto.
+    }
+  });
 }
